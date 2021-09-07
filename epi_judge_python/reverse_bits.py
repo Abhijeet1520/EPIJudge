3d11ee00c90380153e8b7f9d45206cadb7b31f27
Abhijeet Kumar <freelancer.abhijeet.kumar@gmail.com>
2021-09-08 01:40:17 +0530

    Used swap_bits for reverse_bits.py

--- epi_judge_python/reverse_bits.py
+++ epi_judge_python/reverse_bits.py
@@ -1,13 +1,15 @@
 from test_framework import generic_test
 
+def swap_bits(x, i, j):
+    if (x >> i) & 1 != (x >> j) & 1:
+        bit_mask = (1 << i) | (1 << j)
+        x ^= bit_mask
+    return x
 
 def reverse_bits(x: int) -> int:
-    result = 0
-    while x:
-        result <<= 1
-        result ^= (x & 1)
-        x >>= 1
-    return result
+    for i in range(32):
+        x = swap_bits(x, i, 64 - i - 1)
+    return x
 
 
 if __name__ == '__main__':
